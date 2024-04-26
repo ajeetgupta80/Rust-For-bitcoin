a5f7c53814855922ae795b45436d95d91c3ee81a
fucking ajeet <ajeet808080@gmail.com>
2024-04-27 02:54:30 +0530

    files updated

---
+++ hello-cargo/target/debug/deps/hello_cargo-7d2c66544292c5b5.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/AJEET BROCK/Desktop/rust/hello-cargo/target/debug/deps/hello_cargo-7d2c66544292c5b5: src/main.rs
+
+/mnt/c/Users/AJEET BROCK/Desktop/rust/hello-cargo/target/debug/deps/hello_cargo-7d2c66544292c5b5.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/AJEET\ BROCK/Desktop/rust/hello-cargo/target/debug/hello-cargo: /mnt/c/Users/AJEET\ BROCK/Desktop/rust/hello-cargo/src/main.rs
